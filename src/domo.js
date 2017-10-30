83d185bc99c569b5d472184d411fb204c5b0da50
donaldFord <donald.ford@domo.com>
2017-10-30 14:42:16 -0600

    added string check to onDataUpdate

--- src/domo.js
+++ src/domo.js
@@ -63,28 +63,30 @@ domo.getAll = function(urls, options) {
  */
 domo.onDataUpdate = function(cb){
   window.addEventListener('message', function(event) {
-    try {
-      var message = JSON.parse(event.data);
-      if (!message.hasOwnProperty('alias')) {
-        return;
+    if (typeof event.data === 'string') {
+      try {
+        var message = JSON.parse(event.data);
+        if (!message.hasOwnProperty('alias')) {
+          return;
+        }
+
+        var alias = message.alias;
+
+        // send acknowledgement to prevent autorefresh
+        var ack = JSON.stringify({
+          event: 'ack',
+          alias: alias,
+        });
+        event.source.postMessage(ack, event.origin);
+
+        // inform domo app which alias has been updated
+        cb(alias);
+      } catch(err) {
+        var info = 'There was an error in domo.onDataUpdate! It may be that our event listener caught ' +
+                   'a message from another source and tried to parse it, so your update still may have worked. ' +
+                   'If you would like more info, here is the error: \n'
+        console.warn(info, err);
       }
-
-      var alias = message.alias;
-
-      // send acknowledgement to prevent autorefresh
-      var ack = JSON.stringify({
-        event: 'ack',
-        alias: alias,
-      });
-      event.source.postMessage(ack, event.origin);
-
-      // inform domo app which alias has been updated
-      cb(alias);
-    } catch(err) {
-      var info = 'There was an error in domo.onDataUpdate! It may be that our event listener caught ' +
-                 'a message from another source and tried to parse it, so your update still may have worked. ' +
-                 'If you would like more info, here is the error: \n'
-      console.warn(info, err);
     }
   });
 };
