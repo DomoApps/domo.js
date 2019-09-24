f2b36004f160a18f0cd1c9528e3d4238c4626008
Jonathon Meng <jonathon.meng@domo.com>
2019-09-24 14:07:06 -0600

    updated types to include JSON and QueryParams

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,5 @@ export { DomoDataFormats } from './enums/domo-data-formats';
 export { DataFormats } from './enums/data-formats';
 
 export { RequestOptions } from './interfaces/request-options';
+export { QueryParams } from './interfaces/query-params';
+export { Json } from './interfaces/json';
