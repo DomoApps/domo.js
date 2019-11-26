505ab5c9cea221fa6ac3d0267a3367da6259ff20
Trevor Tingey <ttingey@gmail.com>
2019-11-26 14:44:55 -0700

    Update request-body.ts

--- src/models/interfaces/request-body.ts
+++ src/models/interfaces/request-body.ts
@@ -14,4 +14,4 @@ export type XMLHttpRequestBody =
   | URLSearchParams
   | ReadableStream<Uint8Array>;
 
-export type RequestBody = Json | XMLHttpRequestBody | unknown;
+export type RequestBody = unknown;
