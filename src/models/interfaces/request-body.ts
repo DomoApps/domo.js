7acdb6e23a5979165c5f6ff27f1f98d84ee1cea3
JSON <Jason.Hansen@domo.com>
2025-06-23 13:53:19 -0600

    Missed a couple

--- src/models/interfaces/request-body.ts
+++
@@ -1,9 +0,0 @@
-import { DomoDataFormats, RequestOptions, QueryParams, RequestBody, ArrayResponseBody, ObjectResponseBody, ResponseBody } from './request';
-import { Json } from './json';
-/**
- * This is from the XMLHttpRequest Documentation:
- * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/send
- * 2021/10/28: XMLHttpRequest type was added to official Typescript types, using that instead.
- */
-
-export type RequestBody = unknown;
